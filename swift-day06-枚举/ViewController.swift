cf948d40c3ddb178557626ee07f166193329e3b6
万安 <wanan@wanandemacbook-pro-2.local>
2016-04-27 14:00:56 +0800

    Initial Commit

---
+++ swift-day06-枚举/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-day06-枚举
+//
+//  Created by 万安 on 16/4/27.
+//  Copyright © 2016年 penglei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
